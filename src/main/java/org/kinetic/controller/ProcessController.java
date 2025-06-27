9566b14e466e01c9a078d9544d67ef66cf8af996
esdomo <grazini.rodrigo+git@gmail.com>
2025-06-27 03:02:26 -0300

    WIP/feat: process_files * Add Simple synchronized batch job -> to count characters in each file * Add h2 setup for spring batch * Use FileProcessorService instead of FileLoaderService in ProcessController

--- src/main/java/org/kinetic/controller/ProcessController.java
+++ src/main/java/org/kinetic/controller/ProcessController.java
@@ -1,37 +1,32 @@
 package org.kinetic.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.kinetic.service.FileLoaderService;
+import org.kinetic.service.FileProcessorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/process")
 @Slf4j
 public class ProcessController {
 
-    private final FileLoaderService fileLoaderService;
+    private final FileProcessorService fileProcessorService;
 
-    public ProcessController(FileLoaderService fileLoaderService) {
-        this.fileLoaderService = fileLoaderService;
+    public ProcessController(FileProcessorService fileProcessorService) {
+        this.fileProcessorService = fileProcessorService;
     }
 
     @PostMapping("start")
     public ResponseEntity<String> processFiles() {
         log.info("Received request to start document processing.");
         try {
-            List<Path> paths = fileLoaderService.listTextFiles();
-            String fileNames = paths.stream().map(Path::toString).collect(Collectors.joining(", "));
-            return ResponseEntity.ok("Processing files: " + fileNames);
-        } catch (IOException e) {
-            return ResponseEntity.internalServerError().body("Could not load files");
+            Long jobId = fileProcessorService.startBatchJob();
+            return ResponseEntity.ok("Started process with id: " + jobId);
+        } catch (Exception e) {
+            log.error("Job failed", e);
+            return ResponseEntity.internalServerError().body("Failed to process files");
         }
     }
 }
